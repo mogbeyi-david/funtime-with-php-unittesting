9912b7eaae616fad551853b985a91c7ad6b8b398
DavidMogbeyi <davidm@l5lab.com>
2018-10-11 23:27:09 +0100

    namespaced the tests folder

---
+++ tests/unit/SampleTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 11/10/2018
+ * Time: 11:11 PM
+ */
+namespace Test\unit;
+
+class SampleTest extends \PHPUnit\Framework\TestCase
+{
+
+}
