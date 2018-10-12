a8a87ed75192cca8b4bcb79485f7b797f60cc76f
DavidMogbeyi <davidm@l5lab.com>
2018-10-12 10:06:05 +0100

    Added test for the get method on the collection class

---
+++ app/Support/Collection.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 12/10/2018
+ * Time: 10:01 AM
+ */
+
+namespace App\Support;
+
+class Collection
+{
+    protected $items = [];
+
+    public function get()
+    {
+        return $this->items;
+    }
+
+    p
+}
---
+++ tests/unit/CollectionTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 12/10/2018
+ * Time: 9:52 AM
+ */
+
+namespace Test\unit;
+
+use App\Support\Collection;
+
+class CollectionTest extends \PHPUnit\Framework\TestCase
+{
+    public function testInitialCollectionIsEmpty()
+    {
+        $collection = new Collection();
+        $this->assertEmpty($collection->get());
+    }
+
+}
